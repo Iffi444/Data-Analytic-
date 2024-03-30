b35f5192f49a5d8b3213ee765de1d4da91d4ada9
Iftakhar <miftakhar19@gmail.com>
2024-03-29 17:31:18 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.title("HR Data Visualization")
+st.header("Data isuaization is a powerful tool for human resources (HR) and talent development. By creating clear, interactive, and insightful visual representations, HR can monitor workforce demographics, track employee performance, analyze turnover rates, and identify skill gaps.")
+
+st.info("We will provide you information about data in visualization Form or DataFrame")
+
+st.warning("For any Information please contact with HR Dept")
+data = pd.read_csv("hrdata1.csv")
+##data.head(1)
+st.success("File is uploaded")
+
+df_cpy_cmp = data.copy()
+df_cpy_cmp.head(6)
+
+st.sidebar.title("Welcome to HR")
+st.sidebar.image("download.jpg")
+# Display the DataFrame using Streamlit
+st.write("### Displaying the Dataset")
+st.dataframe(df_cpy_cmp)
+
+
+
+
+
+
+
